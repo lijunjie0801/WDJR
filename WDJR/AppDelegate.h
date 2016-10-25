6fd7d1e9bea1c618084d8ef7dd3085f6f076b240
fyaex001 <fyaex001@fyaex001s-mac.local>
2016-10-25 09:23:17 +0800

    Initial Commit

---
+++ WDJR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WDJR
+//
+//  Created by fyaex001 on 2016/10/25.
+//  Copyright © 2016年 fyaex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WDJR/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WDJR
+//
+//  Created by fyaex001 on 2016/10/25.
+//  Copyright © 2016年 fyaex. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
